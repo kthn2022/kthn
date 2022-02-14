6f50e354cb5bbfe1bb5ab46d67425cfccb40deea
kthn2022 <98634983+kthn2022@users.noreply.github.com>
2022-02-15 00:29:37 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+  var hidden = document.getElementById('menu');
+    if (hidden.style.display === 'block') hidden.style.display = 'none';
+  else hidden.style.display = 'block';
+  
+  var main = document.getElementById('mbody');
+   if (main.style.marginLeft == '50%') main.style.marginLeft='0px';
+  else main.style.marginLeft='50%';
+}
