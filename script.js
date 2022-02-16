ac37f90e2bd46016c841051a9a03d02a41009fdb
kthn2022 <98634983+kthn2022@users.noreply.github.com>
2022-02-16 14:17:14 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ function myFunction(x) {
    if (main.style.marginLeft == '220px') main.style.marginLeft='0px';
   else main.style.marginLeft='220px';
 }
+function Function() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("mySearch");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("menu");
+  li = ul.getElementsByTagName("li");
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
